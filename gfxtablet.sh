2c0e3b266b203ae412ba50aa1d5c9a73f31bb5d1
NoTeam Studio <mohamedahmedzaky681@gmail.com>
2026-06-08 21:01:34 +0300

    v1.1
    update for myself..

--- gfxtablet.sh
+++ gfxtablet.sh
@@ -2,18 +2,20 @@
 
 cd "$(dirname "$0")"
 
-IP=$(ip route | grep "src 10.37.141." | awk '{for(i=1;i<=NF;i++) if($i=="src") print $(i+1)}' | head -n 1)
+IP=$(ip route | grep -E "dev enx.*src 10\." | awk '{for(i=1;i<=NF;i++) if($i=="src") print $(i+1)}' | head -n 1)
 
 if [ -z "$IP" ]; then
-	notify-send "Network None" "No IP Connected." -i dialog-warning
-	killall networktablet
-	sleep 1
-	notify-send "vTablet Server" "Stoped." -i dialog-information
+    notify-send "Network None" "No IP Connected." -i dialog-warning
+    killall networktablet
+    sleep 1
+    notify-send "GFX Server" "Stoped." -i dialog-information
 else
-	./networktablet &
-fi
-if [ ./networktablet & ]; then
-	notify-send "Network Info" "Now IP: $IP" -i network-transmit-receive
-	sleep 1
-	notify-send "vTablet Server" "Working Now." -i dialog-information
+    notify-send "Network Info" "Now IP: $IP" -i network-transmit-receive
+    if ! pgrep -x "networktablet" > /dev/null; then
+        ./networktablet &
+        sleep 1
+        notify-send "GFX Server" "Working Now..." -i dialog-information
+    else
+        notify-send "GFX Server" "Already Working." -i dialog-information
+    fi
 fi
